d006b321d2e249a5439e2dd4d833ea21b9cc1dad
Conner <ckrecosk@lakeheadu.ca>
2025-02-09 15:25:19 -0500

    Added proper yaml comments

--- controllers/museums.js
+++ controllers/museums.js
@@ -6,9 +6,14 @@ const router = express.Router();
 
 /**
  * @swagger
- * /api/v1/museums
+ * /api/v1/museums:
  *   get:
- * 
+ *     summary: Retrieve all museums
+ *     responses:
+ *       200:
+ *         description: A list of museums
+ *       404:
+ *         description: Not found
  */
 router.get('/', async (req, res) => {
     let museums = await Museum.find();
@@ -23,19 +28,35 @@ router.get('/', async (req, res) => {
 
 /**
  * @swagger
- * /api/v1/museums
+ * /api/v1/museums/{id}:
  *   get:
- * 
+ *      summary: Find museums by their id
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          schema:
+ *            type: string
+ *            required: true
+ *      responses:
+ *        200:
+ *          description: Returns a single museum
+ *        404:
+ *          description: Not found
  */
 router.get('/:id', async (req, res) => {
-    let museum = await Museum.findById(req.params.id);
+    try {
+        let museum = await Museum.findById(req.params.id);
 
-    // If the object is not found, return 404
-    if (!museum) {
-        return res.status(404).json({err: "No result found"});
+        // If the object is not found, return 404
+        if (!museum) {
+            return res.status(404).json({err: "No result found"});
+        }
+
+        return res.status(200).json(museum);
+    } 
+    catch (err) {
+        return res.status(400).json({err: `Bad Request: ${err}`});
     }
-
-    return res.status(200).json(museum);
 })
 
 
@@ -76,17 +97,44 @@ router.post('/', async (req, res) => {
         return res.status(201).json("Resource Created"); // 201 resource created.
     }
     catch (err) {
-        return res.status(400).json({err: `Bad Request: ${err}`}); // 201 resource created.
+        return res.status(400).json({err: `Bad Request: ${err}`});
     }
 });
 
 
 /**
  * @swagger
- * api/v1/museums
+ * /api/v1/museums/{id}:
  *   put:
- *     summary:
- *       
+ *     summary: Update selected museum from/with request body
+ *     parameters: 
+ *       -name: id
+ *       in: path
+ *       required: true
+ *       schema:
+ *         type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               admissionPrice:
+ *                 type: integer
+ *               location:
+ *                 type: string
+ *     responses:
+ *       204:
+ *         description: Resource created
+ *       400:
+ *         description: Bad Request
+ *       404:
+ *         description: Not found
  */
 router.put('/:id', async (req, res) => {
     try {
@@ -98,7 +146,7 @@ router.put('/:id', async (req, res) => {
         }
 
         // Check that there is a id in the req body, and that it is equal to the id in the url
-        if (req.body._id && req.params.id !== req.body._id.toString()) 
+        if (req.body._id && req.params.id !== req.body._id) 
         {
             // Return the bad request status code
             return res.status(400).json({err: "The ID's don't match"});
@@ -115,22 +163,33 @@ router.put('/:id', async (req, res) => {
 
 /**
  * @swagger
- * api/v1/museums
+ * /api/v1/museums/{id}:
  *   delete:
- *     summary:
+ *     summary: Remove a selected museum
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         schema:
+ *           type: string
+ *           required: true
+ *     responses:
+ *       204:
+ *         description: Resource deleted
+ *       404:
+ *         description: Not found
  */
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => { 
     // Search for the item by id
     let museum = await Museum.findById(req.params.id);
 
     if (!museum) {
-        res.status(404).json({err: "Not found"});
+        return res.status(404).json({err: "Not found"});
     }
 
     // Delete the item
     await Museum.findByIdAndDelete(req.params.id);
 
-    res.status(204).json();
+    return res.status(204).json();
 })
 
 // Export the controller so that the rest of the app can use it
